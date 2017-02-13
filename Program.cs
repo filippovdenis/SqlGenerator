e9edfa8cf0ee25d1edcb68851d4bbcec1652fcf8
Filippov Denis <filippov.denis@gmail.com>
2017-02-13 09:24:57 +0700

    Basic objects

--- Program.cs
+++ Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace SqlGenerator
 {
-    class Program
+
+    public class Program
     {
+        public string GenerateSql(TableHierarchy tableHierarchy, string previousSql)
+        {
+            return $"{previousSql}INSERT {tableHierarchy.TableName};";
+        }
+
         static void Main(string[] args)
         {
+            TableHierarchy tableHier = new TableHierarchy
+            {
+                TableName = "ArticleGroup",
+                PrimaryKey = "ArticleGroupId",
+                childHierarchy = new List<TableHierarchy>
+                {
+                    new TableHierarchy
+                    {
+                        TableName = "Article",
+                        PrimaryKey = "ArticleId",
+                        ForeignKey = "ArticleGroupId"
+                    }
+                }
+            };
+            Console.WriteLine($"{tableHier.TableName} : {tableHier.PrimaryKey} : {tableHier.ForeignKey}");
+            Console.WriteLine(tableHier.childHierarchy.Count);
+            foreach(var childHier in tableHier.childHierarchy)
+            {
+                Console.WriteLine($"{childHier.TableName} : {childHier.PrimaryKey} : {childHier.ForeignKey}");
+            }
+            Console.ReadKey();
         }
     }
 }
